2b613af462c2091493a515f8e492afc8ccfcd8dc
JuanF17 <159088860+JuanF17@users.noreply.github.com>
2024-04-29 17:49:02 -0500

    Update server-celulares.mjs

--- server-celulares.mjs
+++ server-celulares.mjs
@@ -3,14 +3,14 @@ import CiudadesController from './features/cuidades/api/v1/celulares-controller.
 
 const app = express();
 
-// Middleware para devolver responses como JSON
+
 app.use(json());
 
-// Routes
+
 const celularesApiController = new CelularesController();
 app.use('/api/', celularesApiController.getRouter());
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
